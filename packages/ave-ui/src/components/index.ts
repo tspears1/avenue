f58b828140b7ba220d417c2b312fb76c86d14d01
Tim Spears <tspears1@gmail.com>
2026-07-27 15:13:24 -0400

    cleanup

--- packages/ave-ui/src/components/index.ts
+++ packages/ave-ui/src/components/index.ts
@@ -5,6 +5,3 @@ import './image/image.lit'
 export { Button } from './button/button.lit'
 export { Card } from './card/card.lit'
 export { Image } from './image/image.lit'
-import './eyebrow/eyebrow.lit'
-
-export { Eyebrow } from './eyebrow/eyebrow.lit'
